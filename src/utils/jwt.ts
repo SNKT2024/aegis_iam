ae9b4fb088e1f1e78673a1aec896e6d0ee4ad10f
sanketkumbharstu@gmail.com <120247634+SNKT2412@users.noreply.github.com>
2026-05-06 12:51:05 +0530

    Added JWT utility to generate Access and Refresh Token

---
+++ src/utils/jwt.ts
@@ -0,0 +1,34 @@
+import type { JwtPayload, SignOptions } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+// interface for strict payload structure
+interface TokenPayload {
+  userId: string;
+  role: string;
+}
+
+const accessSecret = process.env.JWT_ACCESS_SECRET as string;
+const refreshSecret = process.env.JWT_REFRESH_SECRET as string;
+
+// verfying secrets even before server runs
+if (!accessSecret || !refreshSecret) {
+  throw new Error("JWT secrets are not defined in .env");
+}
+
+// generate access token
+export const generateAccessToken = (payload: TokenPayload): string => {
+  const options: SignOptions = {
+    expiresIn: (process.env.JWT_ACCESS_EXPIRY || "15m") as any,
+  };
+  const token = jwt.sign(payload, accessSecret!, options);
+  return token;
+};
+
+// generate refresh token
+export const generateRefreshToken = (payload: TokenPayload) => {
+  const options: SignOptions = {
+    expiresIn: (process.env.JWT_REFRESH_EXPIRY || "7d") as any,
+  };
+  const token = jwt.sign(payload, refreshSecret!, options);
+  return token;
+};
